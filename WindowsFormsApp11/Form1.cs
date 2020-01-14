7601fc3aa574d89f26a8af32ae017388ac817f9b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-01-14 15:04:15 +0200

    Lisätty Windows Forms -sovellus ratkaisuun (solution).

---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+
+            int summa = a + b;
+            MessageBox.Show("Summa on: " + summa);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nimi = nimiTextBox.Text;
+            string email = sähköpostiTextBox.Text;
+
+            string data = $"{nimi}\r\n{email}\r\n";
+            File.WriteAllText(@"C:\Temp\Data.txt", data);
+        }
+    }
+}
