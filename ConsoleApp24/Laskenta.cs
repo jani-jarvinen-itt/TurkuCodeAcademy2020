282d656060fbff5e29a09f3d336773470ac346ff
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-01-15 13:37:25 +0200

    Lisätty yksikkötestiprojekti ratkaisuun (solution).

---
+++ ConsoleApp24/Laskenta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp24
+{
+    public class Laskenta
+    {
+        public int Summa(int a, int b)
+        {
+            if ((a < 0) || (b < 0))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp24Tests1/LaskentaTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp24;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24.Tests
+{
+    [TestClass()]
+    public class LaskentaTests
+    {
+        [TestMethod()]
+        public void SummaTest()
+        {
+            int a = -1;
+            int b = -2;
+
+            try
+            {
+                Laskenta l = new Laskenta();
+                int summa = l.Summa(a, b);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                if (!(ex is ArgumentOutOfRangeException))
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+    }
+}
